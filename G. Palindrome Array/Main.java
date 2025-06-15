82e8b475bf018179f2b58ed791a8b77ff4442e63
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-06-15 21:09:59 +0600

    .

--- G. Palindrome Array/Main.java
+++ G. Palindrome Array/Main.java
@@ -1,24 +1,31 @@
+// https://codeforces.com/group/MWSDmqGsZm/contest/219774/problem/G
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main{
+public class Main {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
 
         int[] arr = new int[n];
+        int[] revArr = new int[arr.length];
 
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < arr.length; i++){
             arr[i] = sc.nextInt();
         }
 
-        // Palindrome
-        for(int i = 0; i < n; i++){
-            for(int j = i; j < n; j++){
-                if(arr[i] == arr[j] && i != j){
-                    System.out.println(arr[i]);
-                }
-            }
+        for(int i = arr.length - 1, j = 0; i >=0; i--, j++){
+            revArr[j] = arr[i];
         }
+
+        if(Arrays.equals(arr, revArr)){
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
+        }
+
+        sc.close();
     }
 }
