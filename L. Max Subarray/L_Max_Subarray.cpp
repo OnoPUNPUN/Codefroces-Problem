006ca08f2951f1adfbf095a1ec41291be31c8424
TONMOY, WASIMUL BARI <punpunonodera754@gmail.com>
2025-07-06 23:38:56 +0600

    .

---
+++ L. Max Subarray/L_Max_Subarray.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <iostream>
+#include <climits>
+#include <algorithm>
+
+using namespace std;
+
+
+int maxSubarray(int* arr, int n){
+    int maxSum = INT_MIN;
+    int currentSum = 0;
+
+    for(int i = 0; i < n; i++){
+        currentSum += arr[i];
+        maxSum = max(maxSum, currentSum);
+        if(currentSum < 0){
+            currentSum = 0;
+        }
+    }
+    
+    return maxSum;
+}
