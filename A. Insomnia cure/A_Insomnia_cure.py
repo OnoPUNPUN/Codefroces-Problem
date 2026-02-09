ba01e562340873f4b7f7d26a88fadac79c5e5565
OnoPUNPUN <punpunonodera754@gmail.com>
2026-02-09 22:16:16 +0600

    .

---
+++ A. Insomnia cure/A_Insomnia_cure.py
@@ -0,0 +1,15 @@
+
+k = int(input())
+l = int(input())
+m = int(input())
+n = int(input())
+d = int(input())
+
+if k == 1 or l == 1 or m == 1 or n == 1:
+    print(d)
+else:
+    total = d
+    for i in range(1, d + 1):
+        if (i % k != 0) and (i % l != 0) and (i % m != 0) and (i % n != 0):
+            total -= 1
+    print(total)
