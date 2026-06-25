5a2fc02cb7e6dcc6160ebb60b317d5cb206d72c6
OnoPUNPUN <punpunonodera754@gmail.com>
2026-06-25 23:38:49 +0600

    .

---
+++ A. Beautiful Matrix/ABeautifulMatrix.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+/**
+ * ABeautifulMatrix
+ */
+public class ABeautifulMatrix {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[5][5];
+
+        for(int i = 0; i < 5; i++) {
+            for(int j = 0; j < 5; j++) {
+                arr[i][j] = sc.nextInt();
+            }
+        }
+    }
+}
