46355e78ffbaa07033e5c1dc2377eeb9d1c564a2
OnoPUNPUN <punpunonodera754@gmail.com>
2026-06-26 00:00:44 +0600

    A. Beautiful Matrix

--- A. Beautiful Matrix/ABeautifulMatrix.java
+++ A. Beautiful Matrix/ABeautifulMatrix.java
@@ -9,10 +9,19 @@ public class ABeautifulMatrix {
         Scanner sc = new Scanner(System.in);
         int[][] arr = new int[5][5];
 
-        for(int i = 0; i < 5; i++) {
-            for(int j = 0; j < 5; j++) {
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
+                if (arr[i][j] == 1) {
+                    System.out.println(Math.abs(i - 2) + Math.abs(j - 2));
+                }
+            }
+        }
+        sc.close();
     }
 }
